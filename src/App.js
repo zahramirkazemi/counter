6092519e5dbe640d770f7a7eb97aff64ace4c93b
zahramirkazemi <zahramirkazemi1997@gmail.com>
2022-04-12 17:52:08 +0430

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Box, Button, Typography } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions'
 
 function App() {
+  const counter = useSelector(state => state);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: 'center',marginTop: '50px'}}>
+          <Box sx={{bgcolor: '#001e3c',display: 'inline-flex',borderRadius: 4, px: 7, py: 5,boxShadow: 5}}>
+          <Button onClick={ () => dispatch(increment) } variant="contained">Increment</Button>
+          <Typography variant="button" gutterBottom color="common.white" sx={{ display: 'inline', px: 2, m: "auto",fontSize: 'h6.fontSize'}}>
+            {counter}
+          </Typography>
+          <Button onClick={ () => dispatch(decrement)} variant="contained">Decrement</Button>
+          </Box>
     </div>
+
   );
 }
 
